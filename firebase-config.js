add82b73bed4645a5626a53bae977b33957e9fc8
zamnnzz <m.o.a.t1427@gmail.com>
2026-08-26 01:06:18 +0300

    س

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase Web configuration
+// ملاحظة: إذا كان apiKey في النسخة المرسلة مختصرًا بـ xxxxx، استبدله بالقيمة الحقيقية.
+window.firebaseConfig = {
+  apiKey: "AIzaSyxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "horof-game-9934e.firebaseapp.com",
+  databaseURL: "https://horof-game-9934e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "horof-game-9934e",
+  appId: "1:44896634225:web:62a672d8c94be6e0d762d0"
+};
